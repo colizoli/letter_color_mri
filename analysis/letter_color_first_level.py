4c6e9d15c2535e32c058b7303a9749f648275063
Olympia Colizoli <olympia.colizoli@gmail.com>
2022-02-22 13:22:50 +0100

    Add files via upload

---
+++ analysis/letter_color_first_level.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+first_level.py
+Created by O.Colizoli, June-2019
+Last update: 11-06-2019
+Python version 2.7
+
+The following packages need to be installed because they are called from the command line, but not imported:
+fsl
+
+"""
+
+# TO DO:
+# bids output files: sub, session, task, run, filetypebids coverted does latter...
+# run-01 or run-1?? bids coverted does latter...
+
+import os, subprocess, sys
+import shutil as sh
+import nibabel as nib
+import pandas as pd
+from IPython import embed as shell # for Oly's debugging only
+
+class preprocess_class(object):
+    def __init__(self, subject, session, analysis_dir, deriv_dir, mask_dir, template_dir, timing_files_dir):        
+        self.subject        = 'sub-'+str(subject)
+        self.session        = str(session)
+        self.analysis_dir   = str(analysis_dir)
+        self.deriv_dir      = str(deriv_dir)
+        self.mask_dir       = str(mask_dir)
+        self.template_dir   = str(template_dir)
+        self.timing_files_dir = str(timing_files_dir)
+        
+        if not os.path.isdir(self.source_dir):
+            os.mkdir(self.source_dir)
+        if not os.path.isdir(self.deriv_dir):
+            os.mkdir(self.deriv_dir)
+        if not os.path.isdir(self.mask_dir):
+            os.mkdir(self.mask_dir)
+        if not os.path.isdir(self.template_dir):
+            os.mkdir(self.template_dir)
+        if not os.path.isdir(self.timing_files_dir):
+            os.mkdir(self.timing_files_dir)   
+    
+
+    def loc_timing_files(self, task, runs):
+        # GLM timing files for localizers
+        # ABAB blocked designs
+    
+        stim_dur = 0.75 # stimulus duration seconds
+    
+        for run in runs:
+            onsets = pd.read_csv(os.path.join(deriv_dir,subject,session,'events','{}_task-{}_{}_run-{}.csv'.format(subject,task, session, run)))
+            onsets.drop(['Unnamed: 0'], axis=1, inplace=True) 
+    
+            for c,cond in enumerate(np.unique(onsets['condition'])):
+                outFile = os.path.join(deriv_dir,'timing_files','task-{}_{}_{}_run-{}_{}.txt'.format(task,subject,session,run,cond))
+
+                # main regressors
+                first = np.array(onsets[onsets['condition']==cond]['onset_time'])
+                second = np.repeat(stim_dur, len(first))
+                third = np.array(np.repeat(1, len(first)),dtype=int)
+                output = np.array(np.vstack((first, second, third)).T ,dtype=float) # 1 x 3
+                f = open(outFile,'w')
+                np.savetxt(f, output, fmt=['%.2f', '%.2f', '%i']) # doesn't work python 3
+            
+                f.close()
+        
+        print('success: loc_timing_files {} {} {} run-{}'.format(task,subject,session,run))     
+
+    def rsa_timing_files(task, subject, session, runs):
+        # GLM timing files for localizers
+        # ABAB blocked designs
+    
+        stim_dur = 1.5 # stimulus duration seconds
+    
+        for run in runs:
+            onsets = pd.read_csv(os.path.join(deriv_dir,subject,session,'events','{}_task-{}_{}_run-{}.csv'.format(subject,task, session,run)))
+            onsets.drop(['Unnamed: 0'], axis=1, inplace=True) 
+    
+            for c,cond in enumerate(np.unique(onsets['color_condition'])):
+                outFile = os.path.join(deriv_dir,'timing_files','task-{}_{}_{}_run-{}_{}.txt'.format(task,subject,session,run,cond))
+
+                # main regressors
+                first = np.array(onsets[onsets['color_condition']==cond]['onset_time'])
+                second = np.repeat(stim_dur, len(first))
+                third = np.repeat(1, len(first))
+                output = np.vstack((first, second, third)).T # 1 x 3
+                f = open(outFile,'w')
+                np.savetxt(f, output, fmt=['%.2f', '%.2f', '%i'])
+                f.close()
+        
+        print('success: rsa_timing_files {} {} {} run-{}'.format(task,subject,session,run))    
+
+    def loc_fsf(task, subject, session, runs):
+        # Creates the FSF files for each subject's first level analysis
+        # localizers
+
+        rm_TRs = str(5) # TRs to trim in beginning
+        fwhm = str(3) # smoothing kernel mm
+    
+        # template_filename = os.path.join(deriv_dir,'first_level','templates','task-{}_template.fsf'.format(task))
+        template_filename = os.path.join(deriv_dir,'first_level','templates','task-{}_template_B0_False.fsf'.format(task))
+    
+        markers = [
+            '[$OUTPUT_PATH]', 
+            '[$TR]',
+            '[$NR_TRS]', 
+            '[$REMOVE_TRS]',    # number of TRs to remove
+            '[$DWELL_TIME]',    # EPI DWELL TIME, EFFECTIVE ECHO SPACING
+            '[$ECHO_TIME]',     # EPI echo time
+            '[$FWHM]',          # smoothing kernel
+            '[$INPUT_FILENAME]', 
+            '[$FMAP]',
+            '[$FMAP_MAG_BRAIN]',
+            '[$T1]',
+            '[$EV1_FILENAME]',
+            '[$EV2_FILENAME]', 
+            '[$NR_VOXELS]'
+        ]
+    
+        for r,run in enumerate(runs):        
+            BOLD = os.path.join(deriv_dir,subject,session,'func','{}_task-{}_run-{}_bold.nii.gz'.format(subject,task,run))
+            # calculate size of input data
+            nii = nib.load(BOLD).get_data() # only do once 
+            nr_trs = str(nii.shape[-1])
+            nr_voxels = str(nii.size)
+                
+            FSF_filename = os.path.join(deriv_dir,'first_level','task-{}'.format(task),'task-{}_{}_{}_run-{}.fsf'.format(task,subject,session,run) ) # save fsf
+            # replacements
+            output_path = os.path.join(deriv_dir,'first_level','task-{}'.format(task),'task-{}_{}_{}_run-{}'.format(task,subject,session,run) ) 
+            EV1_path = os.path.join(deriv_dir,'timing_files','task-{}_{}_{}_run-{}_{}.txt'.format(task,subject,session,run,'black'))
+            EV2_path = os.path.join(deriv_dir,'timing_files','task-{}_{}_{}_run-{}_{}.txt'.format(task,subject,session,run,'color'))
+    
+            T1 = os.path.join(deriv_dir,subject,session,'anat','{}_T1w_brain.nii.gz'.format(subject))
+            FMAP = os.path.join(deriv_dir,subject,session,'fmap','{}_acq-fmap.nii.gz'.format(subject))
+            FMAP_MAG_BRAIN = os.path.join(deriv_dir,subject,session,'fmap','{}_fmap_run-01_fmap_brain.nii.gz'.format(subject))
+            dwell_time = str(effective_EPI_echo_spacing[r])
+            echo_time = str(echo_times[r])
+            TR = str(TRs[r])
+        
+            replacements = [ # needs to match order of 'markers'
+                output_path,
+                TR,
+                nr_trs,
+                rm_TRs,
+                dwell_time,
+                echo_time,
+                fwhm,
+                BOLD,
+                FMAP,
+                FMAP_MAG_BRAIN,
+                T1,
+                EV1_path,
+                EV2_path,
+                nr_voxels
+            ]
+    
+            # open the template file, load the text data
+            f = open(template_filename,'r')
+            filedata = f.read()
+            f.close()
+
+            # search and replace
+            for st,this_string in enumerate(markers):
+                filedata = filedata.replace(this_string,replacements[st])
+    
+            # write output file
+            f = open(FSF_filename,'w')
+            f.write(filedata)
+            f.close()
+            print('success: {}_fsf {} {} run-{}'.format(task,subject,session,run))  
+    
+# ----------------------
+# RUN FUNCTIONS
+# ----------------------
+subjects = ['sub-503']
+sessions = ['sess-01']
+color_runs = [
+    ['01','02'] # multiband6, multiband4
+]
+rsa_runs = [
+    ['1','2']
+]
+letter_runs = [
+    ['1','2'] # multiband6, multiband4
+]
+vwfa_runs = [
+    ['1','2'] # multiband6, multiband4
+]
+
+# TRs = [1.0,1.5]
+TRs = [1.5,1.5]
+
+
+#### B0 unwarping ####
+# current issue: FEAT asks for echo time to run unwarping, but what is the echo time for the combined images?
+# however, when unwarping run from FUGUE, it only asks for dwell time and magnitude image.
+# what is FEAT doing differently from FUGUE? (combining with motion correct and BBR, but why is echo time needed?)
+# 'dwell_time' refers to effective echo spacing of EPI data
+echo_tdiff = 0.00246 # seconds, difference between echo times of field map magnitude images [0.0047,0.00716]
+echo_times = [34.00, 39.60]
+effective_EPI_echo_spacing = [0.000619987,0.000580009]*1000 # seconds, 'dwell time', param in json, corrected for phase encoding acceleration (not multiband acceleration)
+unwarp_dir = 'y-' # A >> P, 'y-' as input
+signal_loss_thresh = 10 # % default value
+
+
+
+
+
+
+
+
---
+++ analysis/letter_color_preprocessing.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+letter_color_preprocessing.py
+Created by O.Colizoli, June-2019
+Last update: 11-06-2019
+Python version 3.6
+
+The following packages need to be installed because they are called from the command line, but not imported:
+dcm2niix (conda install -c conda-forge dcm2niix)
+dcm2bids (python -m pip install dcm2bids)
+fsl
+
+"""
+
+import os, subprocess, sys
+import shutil as sh
+import nibabel as nib
+import pandas as pd
+from IPython import embed as shell # for Oly's debugging only
+
+class preprocess_class(object):
+    def __init__(self, subject, mri_subject, session, analysis_dir, source_dir, raw_dir, deriv_dir, mask_dir, template_dir, timing_files_dir):        
+        self.subject        = 'sub-'+str(subject)
+        self.mri_subject    = 'sub-'+str(mri_subject)
+        self.session        = str(session)
+        self.analysis_dir   = str(analysis_dir)
+        self.source_dir     = str(source_dir)   # dicoms
+        self.raw_dir        = str(raw_dir)      # raw niftis
+        self.deriv_dir      = str(deriv_dir)
+        self.mask_dir       = str(mask_dir)
+        self.template_dir   = str(template_dir)
+        self.timing_files_dir = str(timing_files_dir)
+            
+        if not os.path.isdir(self.raw_dir):
+            os.mkdir(self.raw_dir)
+        if not os.path.isdir(self.deriv_dir):
+            os.mkdir(self.deriv_dir)
+        if not os.path.isdir(self.mask_dir):
+            os.mkdir(self.mask_dir)
+        if not os.path.isdir(self.template_dir):
+            os.mkdir(self.template_dir)
+        if not os.path.isdir(self.timing_files_dir):
+            os.mkdir(self.timing_files_dir)    
+
+    
+    def dicom2bids(self, ):
+        """ Converts MRI data from dicom to nifti format, then structures them according to the bids standard. 
+        Takes about 15 minutes per session.
+        Requirements:
+            dcm2niix (conda install -c conda-forge dcm2niix)
+            dcm2bids (python -m pip install dcm2bids) 
+        """
+        # for coverting from source directories to bids file names
+        if self.session == 'ses-01':
+            mri_session =  'ses-mri01'
+        else:
+            mri_session = 'ses-mri02'
+        
+        config = 'convert2bids_letter-color_main_{}.json'.format(self.subject)
+        
+        DICOM_DIR = os.path.join(self.source_dir, self.mri_subject, mri_session) #/Users/olympiacolizoli/Aeneas/mountpoint6/raw
+        CONFIG_FILE  = os.path.join(self.analysis_dir, 'config', config)
+        OUTPUT_DIR = os.path.join(self.raw_dir, 'nifti')
+
+        # Create folder structure if not yet existing
+        if not os.path.isdir(os.path.join(self.raw_dir, 'nifti')):
+            os.mkdir(os.path.join(self.raw_dir, 'nifti'))
+
+        cmd = 'dcm2bids -d {} -p {} -s {} -c {} -o {}'.format(DICOM_DIR, self.subject, self.session, CONFIG_FILE, OUTPUT_DIR)
+        results = subprocess.call(cmd, shell=True, bufsize=0)
+        print('converting sub-{} convert2bids_letter-color_main_{}.json'.format(self.mri_subject,self.subject))
+        print('success: dicom2bids')
+        
+    def housekeeping(self, ):
+        """ Copies events and log files into the bids_raw directory with the nifti files
+        Renames the log files to match MRI files: e.g., sess-2 -> ses-02
+        
+        """
+        if self.session == 'ses-01':
+            behav_sess = 'sess-1'
+        else:
+            behav_sess = 'sess-2'
+            
+        logfiles_dir = os.path.join(self.raw_dir, 'logfiles')
+        
+        ###################
+        # copy colors file
+        ###################
+        # try:
+        #     src = os.path.join(self.raw_dir, 'logfiles', self.subject, '{}_colors.tsv'.format(self.subject))
+        #     dst = os.path.join(self.raw_dir, 'nifti', self.subject, '{}_colors.tsv'.format(self.subject))
+        #     sh.copyfile(src, dst)
+        #     print('colors: src={} , dst={}'.format(src,dst))
+        # except:
+        #     pass
+        
+        ###################
+        # RENAME 'behav' 'sess-1' to 'ses-01' in logfiles/behav folder before copying
+        ###################
+        dir_path = os.path.join(self.raw_dir, 'logfiles', self.subject, behav_sess, 'behav')
+        for F in os.listdir(dir_path):
+            if behav_sess in F:
+                F_new = F.replace(behav_sess, self.session)
+                os.rename(os.path.join(dir_path,F),os.path.join(dir_path,F_new)) # old,new
+                print('old={} , new={}'.format(F,F_new))
+            
+        ###################
+        # copy 'behav' folder
+        ###################
+        # try:
+        #     src = os.path.join(self.raw_dir, 'logfiles', self.subject, behav_sess, 'behav')
+        #     dst = os.path.join(self.raw_dir, 'nifti', self.subject, self.session, 'behav')
+        #     sh.copytree(src, dst)
+        #     print('behav: src={} , dst={}'.format(src,dst))
+        # except:
+        #     pass
+        
+        ###################
+        # RENAME 'func' 'sess-1' to 'ses-01' in logfiles/func folder before copying
+        # then runs 'run-1' to 'run-01' etc
+        ###################
+        dir_path = os.path.join(self.raw_dir, 'logfiles', self.subject, behav_sess, 'func')
+        behav_runs = ['run-1','run-2','run-3','run-4']
+        bids_runs = ['run-01','run-02','run-03','run-04']
+        # loops through functional files
+        for F in os.listdir(dir_path):
+            # replace sessions
+            if behav_sess in F:
+                F_new = F.replace(behav_sess, self.session)
+                os.rename(os.path.join(dir_path,F),os.path.join(dir_path,F_new)) # old,new
+                print('old={} , new={}'.format(F,F_new))
+        # restart loop because new session changed names
+        for F in os.listdir(dir_path):
+            # replaces runs
+            for r,this_run in enumerate(behav_runs):
+                if this_run in F:
+                    F_new = F.replace(this_run, bids_runs[r])
+                    os.rename(os.path.join(dir_path,F),os.path.join(dir_path,F_new)) # old,new
+                    print('old={} , new={}'.format(F,F_new))
+        
+        ###################
+        # RENAME localizer runs only: remove '_run-01' because only 1 run of each
+        ###################
+        dir_path = os.path.join(self.raw_dir, 'logfiles', self.subject, behav_sess, 'func')
+        # loops through functional files
+        for F in os.listdir(dir_path):
+            # remove run from localizer event file names
+            if ('task-colorsloc' in F) or ('task-lettersloc' in F):
+                F_new = F.replace('_run-01', '')
+                os.rename(os.path.join(dir_path,F),os.path.join(dir_path,F_new)) # old,new
+                print('old={} , new={}'.format(F,F_new))
+                try: # some files have runs 5 or 6
+                    F_new = F.replace('_run-5', '')
+                    os.rename(os.path.join(dir_path,F),os.path.join(dir_path,F_new)) # old,new
+                    F_new = F.replace('_run-6', '')
+                    os.rename(os.path.join(dir_path,F),os.path.join(dir_path,F_new)) # old,new
+                except:
+                    pass
+                    
+        ###################
+        # copy mri event files into existing 'func' folder
+        ###################
+        # try:
+        #     # loop through functional files
+        #     for F in os.listdir(os.path.join(self.raw_dir, 'logfiles', self.subject, behav_sess, 'func')):
+        #         src = os.path.join(self.raw_dir, 'logfiles', self.subject, behav_sess, 'func', F)
+        #         dst = os.path.join(self.raw_dir, 'nifti', self.subject, self.session, 'func', F)
+        #         sh.copy(src, dst)
+        #     print('func: src={} , dst={}'.format(src,dst))
+        # except:
+        #     pass
+           
+        print('success: housekeeping')
+    
+
+    def raw_copy(self,):
+        """ copy from bids_raw directory into derivaties folder for further processing/analysis.
+        All further analysis should be run within the derivatives folder.
+        """
+        if os.path.isdir(os.path.join(self.deriv_dir, self.subject, self.session)):
+            print('Target folder already exists. No files will be copied. Delete existing session from derivatives if intended.')
+            return
+        else:
+            # Copy nifti
+            src = os.path.join(self.raw_dir, 'nifti', self.subject, self.session)
+            dst = os.path.join(self.deriv_dir, self.subject, self.session)
+            sh.copytree(src, dst)
+        print('success: source_copy')
+
+    def bet_brains_T1(self, postfix='brain'):
+        """ Runs brain extraction on all T1s.
+        Always check visually in fsleyes.
+        """
+    
+        inFile = os.path.join(self.deriv_dir, self.subject, 'sess-01', 'anat', '{}_T1w.nii.gz'.format(self.subject))
+        outFile = os.path.join(self.deriv_dir, self.subject, 'sess-01', 'anat', '{}_T1w_{}.nii.gz'.format(self.subject, postfix))
+        # bet inFile outFile
+        cmd = 'bet {} {}'.format(inFile, outFile)
+        print(cmd)
+        results = subprocess.call(cmd, shell=True, bufsize=0)
+
+        # reorient to mni space if necessary (sometimes turned around)
+        # cmd = 'fslreorient2std {} {}'.format(outFile,outFile)
+        # proc = subprocess.call( cmd, shell=True, bufsize=0,) # COMMAND LINE
+        print('success: bet_brains_T1')
+    
+    def bet_brains_fmap(self, postfix='brain'):
+        # Runs brain extraction on magnitude images of field maps
+        # This needs to be 'tight', meaning it is better to exclude brain voxels than include noisy non-brain voxels
+        # Important! Always check visually in fsleyes
+
+        for run in ['01','02','03']: # first 2 are magnitude, 3rd is phase
+            inFile = os.path.join(self.deriv_dir,  self.subject,  self.session, 'fmap', '{}_fmap_run-{}_fmap.nii.gz'.format(self.subject,run))
+            outFile = os.path.join(self.deriv_dir,  self.subject,  self.session, 'fmap', '{}_fmap_run-{}_fmap_{}.nii.gz'.format(self.subject,run,postfix))
+            # bet inFile outFile
+            cmd = 'bet {} {} -f 0.6'.format(inFile, outFile) 
+            print(cmd)
+            results = subprocess.call(cmd, shell=True, bufsize=0)
+    
+            # reorient to mni space if necessary (sometimes turned around)
+            # cmd = 'fslreorient2std {} {}'.format(outFile,outFile)
+            # proc = subprocess.call( cmd, shell=True, bufsize=0,) # COMMAND LINE
+        print('success: bet_brains_fmap')
+    
+    def prepare_fmap(self, echo_tdiff=0.00246):
+        """ Need to prepare field map before B0 unwarping
+        https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FUGUE/Guide#SIEMENS_data
+        Run after brain extraction on magnitude image
+        Note too important which magnitude image is chosen
+        Echo time difference need to be in ms (echotimes in JSON)
+        fsl_prepare_fieldmap <scanner> <phase_image> <magnitude_image> <out_image> <deltaTE (in ms)> [--nocheck]
+        The output of the Fsl_prepare_fieldmap GUI is a correctly calibrated fieldmap in units of rad/s     
+        'dwell_time' refers to effective echo spacing of EPI data
+        echo_tdiff = 0.00246 # seconds, difference between echo times of field map magnitude images [0.0047,0.00716]
+        """
+    
+        echo_tdiff = echo_tdiff * 1000 # needs to be in milliseconds
+        phase_image = os.path.join(self.deriv_dir, self.subject, self.session, 'fmap', '{}_fmap_run-03_fmap.nii.gz'.format(self.subject))
+        mag_image = os.path.join(self.deriv_dir, self.subject, self.session, 'fmap', '{}_fmap_run-01_fmap_brain.nii.gz'.format(self.subject))
+        outFile = os.path.join(self.deriv_dir, self.subject, self.session, 'fmap', '{}_acq-fmap.nii.gz'.format(self.subject))
+        # bet inFile outFile
+        cmd = 'fsl_prepare_fieldmap {} {} {} {} {} [--nocheck]'.format('SIEMENS',phase_image,mag_image,outFile,echo_tdiff)
+        print(cmd)
+        results = subprocess.call(cmd, shell=True, bufsize=0)
+        print('success: prepare_fmap')
